883cb69ab78bcf45111ae35a8e01e0705457db53
NTMy98 <nguyenthuymy2009@gmail.com>
2024-12-21 08:53:48 +0700

    login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -22,3 +22,44 @@ it("Successful login for user", () => {
   })
 })
 
+it("Can't login when missing both email and password", () => {
+  loginPage.clickSignInButton()
+
+  cy.get('.Toastify__toast-body').should("contain", "Invalid email or password")
+
+})
+
+it("Can't login when missing email", () => {
+  cy.get(loginPage.TXT_PASSWORD).type("123456")
+  loginPage.clickSignInButton()
+
+  cy.get('.Toastify__toast-body').should("contain", "Invalid email or password")
+})
+
+it("Can't login when missing password", () => {
+  cy.get(loginPage.TXT_EMAIL).type("admin@email.com")
+  loginPage.clickSignInButton()
+
+  cy.get('.Toastify__toast-body').should("contain", "Invalid email or password")
+})
+
+it("Can't login when using information not exists in system", () => {
+  cy.get(loginPage.TXT_EMAIL).type("tester0123@gmail.com")
+  cy.get(loginPage.TXT_PASSWORD).type("AbcdEf123456")
+  loginPage.clickSignInButton()
+
+  cy.get('.Toastify__toast-body').should("contain", "Invalid email or password")
+})
+
+it("Can login when adding space character before and after email", () => {
+  cy.get("@user").then((user) => {
+    cy.get(loginPage.TXT_EMAIL).type("          admin@email.com          ")
+    loginPage.typePassword(user.admin.password).clickSignInButton() 
+    homePage.checkDirectToHomePage(user.admin.username)
+  })
+})
+
+it("Go to register page successful", () => {
+    loginPage.clickRegisterButton()
+    cy.url().should("include", "/register");
+})
